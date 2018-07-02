cdde944f7349129d89b14a2f4c5683b3a5329461
Ramesh Babu <s.rameshbabu1989@gmail.com>
2018-07-02 08:28:59 +0530

    Using Interface to implement the OC Principle

--- Principle/OCP/src/com/ramesh/Main.java
+++ Principle/OCP/src/com/ramesh/Main.java
@@ -1,8 +1,125 @@
 package com.ramesh;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+enum Color
+{
+    RED, GREEN, BLUE
+}
+
+enum Size
+{
+    SMALL, MEDIUM, LARGE
+}
+
+class Product
+{
+    public String name;
+    public Color color;
+    public Size size;
+
+    public Product(String name, Color color, Size size) {
+        this.name = name;
+        this.color = color;
+        this.size = size;
+    }
+}
+
+interface Specification<T>
+{
+    boolean isSatisfied(T item);
+}
+
+interface Filter<T>
+{
+    Stream<T> filter(List<T> items, Specification<T> spec);
+}
+
+class ColorSpecification implements Specification<Product>
+{
+    private Color color;
+
+    public ColorSpecification(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean isSatisfied(Product p) {
+        return p.color == color;
+    }
+}
+
+
+class SizeSpecification implements Specification<Product>
+{
+    private Size size;
+
+    public SizeSpecification(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean isSatisfied(Product p) {
+        return p.size == size;
+    }
+}
+
+class AndSpecification<T> implements Specification<T>
+{
+    private Specification<T> first, second;
+
+    public AndSpecification(Specification<T> first, Specification<T> second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean isSatisfied(T item) {
+        return first.isSatisfied(item) && second.isSatisfied(item);
+    }
+
+}
+
+class BetterFilter implements Filter<Product>
+{
+    @Override
+    public Stream<Product> filter(List<Product> items, Specification<Product> spec) {
+        return items.stream().filter(p -> spec.isSatisfied(p));
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Product apple = new Product("Apple", Color.GREEN, Size.SMALL);
+        Product tree = new Product("Tree", Color.GREEN, Size.LARGE);
+        Product house = new Product("House", Color.BLUE, Size.LARGE);
+
+        List<Product> products = new ArrayList<Product>();
+        products.add(apple);
+        products.add(tree);
+        products.add(house);
+
+        BetterFilter bf = new BetterFilter();
+        System.out.println("Green products (new):");
+        bf.filter(products, new ColorSpecification(Color.GREEN))
+                .forEach(p -> System.out.println(" - " + p.name + " is green"));
+
+        System.out.println("Large products:");
+        bf.filter(products, new SizeSpecification(Size.LARGE))
+                .forEach(p -> System.out.println(" - " + p.name + " is large"));
+
+        System.out.println("Large blue items:");
+        bf.filter(products,
+                new AndSpecification<>(
+                        new ColorSpecification(Color.BLUE),
+                        new SizeSpecification(Size.LARGE)
+                ))
+                .forEach(p -> System.out.println(" - " + p.name + " is large and blue"));
+
+
     }
 }
